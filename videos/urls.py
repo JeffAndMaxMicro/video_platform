de8ba81df679ce61b226c3018a2ba35e5e755cb3
Jeff Wang <>
2024-05-17 23:36:44 +0800

    get videos

---
+++ videos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('upload/', views.VideoUpload, name='video-upload'),
+    path('', views.VideoList, name='video-list'),
+]
--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import generics
+from .models import Videos
+from .serializers import VideoSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+def VideoUpload(request):
+    pass
+
+def VideoList(request):
+    videos = Videos.objects.all()
+    serializer = VideoSerializer(videos, many=True)
+    return JsonResponse(serializer.data, safe=False)
